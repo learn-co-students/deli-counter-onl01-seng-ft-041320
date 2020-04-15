e0d467d0641061d880091e7f264ea74aa392d9d3
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-04-15 16:45:00 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ end
 end
   
   def now_serving(katz_deli)
-  if katz_deli.empty?
+  if katz_deli < 0
     puts "There is nobody waiting to be served!"
   else
  puts "Currently serving #{katz_deli.first}."
