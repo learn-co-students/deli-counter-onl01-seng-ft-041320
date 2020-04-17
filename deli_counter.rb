2ab7e889e033f16d93fe53843b834f6202d4893a
Michael Piercy <michael.piercy@zoho.com>
2020-04-17 22:27:25 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+katz_deli = []
+
+def line(cust)
+  line_array = []
+  if cust.length == 0
+  puts "The line is currently empty."
+  else
+  cust.each.with_index(1) do |name, position|
+  line_array.push("#{position}. #{name}")
+  end
+  puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, cust)
+  katz_deli.push(cust)
+  puts "Welcome, #{cust}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(cust)
+  if cust.empty?
+  puts "There is nobody waiting to be served!"
+  else
+  puts "Currently serving #{cust[0]}."
+  cust.shift
+  end
+end
