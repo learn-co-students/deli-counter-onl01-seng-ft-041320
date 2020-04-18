0d89a19bfdad0b88e5beda51cd944456796248d7
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-18 21:07:33 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,10 @@ def line(array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    num = 1
-    puts ""
+    array.each do
+    # put a counter to increment the line number for each do, 
+    # concat the strings to create the return_string
+    end
   end
 end
 
@@ -13,6 +15,10 @@ def take_a_number(array, name)
 end
 
 def now_serving(array)
-  puts "Currently serving " + array.take(1) + "."
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+else array.length >= 1 
+  puts "Currently serving " + array.take(1).join + "."
   array = array.drop(1)
+end
 end
