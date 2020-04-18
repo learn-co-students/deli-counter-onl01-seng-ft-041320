5767769d73efdca6803ddf505ac9cfabf2784a7e
Jaeson Watts <jaeson.watts@gmail.com>
2020-04-18 08:28:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    num = 1
+    puts ""
+  end
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving(array)
+  puts "Currently serving " + array.take(1) + "."
+  array = array.drop(1)
+end
